ecabf42d436b6a7defb1ff5c7b00bc475320c0ed
孙晓毅 <1303601369@qq.com>
2021-05-28 18:04:43 +0800

    正则表达式

--- str/src/str/RegularExpression.java
+++ str/src/str/RegularExpression.java
@@ -54,35 +54,8 @@ public class RegularExpression {
         dp
      */
     public boolean isMatch2(String s, String p) {
-        int m = s.length();
-        int n = p.length();
-        boolean[][] f = new boolean[m + 1][n + 1];
-        f[0][0] = true;
-        for (int i = 0; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                if (p.charAt(j - 1) == '*') {
-                    f[i][j] = f[i][j - 2];
-                    if (matches(s, p, i, j - 1)) {
-                        f[i][j] = f[i][j] || f[i - 1][j];
-                    }
-                } else {
-                    if (matches(s, p, i, j)) {
-                        f[i][j] = f[i - 1][j - 1];
-                    }
-                }
-            }
-        }
-        return f[m][n];
-    }
-
-    public boolean matches(String s, String p, int i, int j) {
-        if (i == 0) {
-            return false;
-        }
-        if (p.charAt(j - 1) == '.') {
-            return true;
-        }
-        return s.charAt(i - 1) == p.charAt(j - 1);
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        return false;
     }
 
 }
